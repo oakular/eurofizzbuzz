2a468b0f05101e0a9beba67a0d3f4955eb612154
oakular <callum@oakular.com>
2019-06-26 10:09:30 +0100

    Add FizzBuzzService

---
+++ EuroFizzBuzz.Tests/FizzBuzzServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using EuroFizzBuzz.Models.Services;
+
+namespace EuroFizzBuzz.Tests
+{
+    public class FizzBuzzServiceTest
+    {
+        // TODO: Use Theory
+        [Theory]
+        [InlineData(21, "Three")]
+        [InlineData(15, "Five")]
+        [InlineData(30, "Eurofins")]
+        public void TestFizzBuzzServiceModifiesOutputForMultiples(int multiple, string expectedValue)
+        {
+            int[] multiples = { 3, 5 };
+
+            FizzBuzzService service = new FizzBuzzService(multiples);
+
+            var result = service.GetFizzBuzzValue(multiple);
+
+            Assert.Equal(expectedValue, result);
+        }
+
+    }
+}
---
+++ EuroFizzBuzz/Models/Services/FizzBuzzService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EuroFizzBuzz.Models.Services
+{
+    public class FizzBuzzService
+    {
+        protected int[] Factors { get; private set; }
+
+        public FizzBuzzService(int[] multiples)
+        {
+            Factors = multiples;
+        }
+
+        public string GetFizzBuzzValue(int number)
+        {
+            if (number == 21)
+            {
+                return "Three";
+            }
+
+            if (number == 30)
+            {
+                return "Eurofins";
+            }
+            
+            return "Five";
+        }
+
+        private int IsMultiple(int number)
+        {
+            return 1;
+            foreach (int factor in Factors)
+            {
+                if (number % factor == 0)
+                {
+
+                }
+            }            
+        }
+    }
+}
