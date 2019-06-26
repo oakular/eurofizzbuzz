d469b0cf454b7c84eafa57918137ef5999f515ff
oakular <callum@oakular.com>
2019-06-26 16:21:14 +0100

    Add route for updating data

--- EuroFizzBuzz/Startup.cs
+++ EuroFizzBuzz/Startup.cs
@@ -63,6 +63,10 @@ namespace EuroFizzBuzz
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "update",
+                    template: "{controller=Home}/{action=Update}/{startNumber?}/{endNumber?}"
+                );
             });
         }
     }
